e66a1c6da93cb65eb803c488213da9af22ccf1fd
Waks Kenneth <42246236+Engwaksken@users.noreply.github.com>
2026-09-15 18:54:34 +0300

    Type church locator service responses

--- lib/services/church_locator_service.dart
+++ lib/services/church_locator_service.dart
@@ -1,2 +1,31 @@
 import 'package:cou_youth_mobile/core/api/api_client.dart';
-class ChurchLocatorService {ChurchLocatorService(this.api); final ApiClient api; Future<dynamic> search(String q)=>api.get('/church-locator?q=${Uri.encodeQueryComponent(q)}');}
+
+class ChurchLocatorService {
+  ChurchLocatorService(this.api);
+
+  final ApiClient api;
+
+  Future<List<Map<String, dynamic>>> search(String query) async {
+    final response = await api.get(
+      '/church-locator?q=${Uri.encodeQueryComponent(query.trim())}',
+    );
+
+    final data = response['data'];
+
+    if (data is List) {
+      return data
+          .whereType<Map>()
+          .map((item) => Map<String, dynamic>.from(item))
+          .toList();
+    }
+
+    if (data is Map && data['data'] is List) {
+      return (data['data'] as List)
+          .whereType<Map>()
+          .map((item) => Map<String, dynamic>.from(item))
+          .toList();
+    }
+
+    return <Map<String, dynamic>>[];
+  }
+}
